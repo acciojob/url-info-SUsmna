a0660e6a1d080bfb85c978c832f20769211358ca
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-28 11:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
+
 const currentURL = location.href;
-
 const urlLength = currentURL.length;
+alert("The length of the URL is: " + urlLength);
 
-alert("The length of the URL is: " + urlLength + ".");
+
